6799daf9121755b158c2f503e37dea8a5276bb38
Anders Lorentzen <anders@Anderss-MacBook-Pro.local>
2025-11-06 14:09:58 +0100

    i hope this is a PR

--- blocks/index.ts
+++ blocks/index.ts
@@ -8,3 +8,5 @@
 // Custom Blocks (migrated to plugin: parsnip-blocks)
 // - hero, gallery, quote are now managed in the parsnip-blocks plugin.
 // If you need to develop theme-local blocks, re-add imports here.
+
+// HUZZAH!!!!
